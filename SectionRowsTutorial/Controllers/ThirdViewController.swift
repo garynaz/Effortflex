0f83ef2adff4b3c578e6822f664d42df334f7f32
garynaz <garynaz@icloud.com>
2020-01-08 02:16:30 -0500

    Working on Third VC layout. - Finished Notes View.

--- SectionRowsTutorial/Controllers/ThirdViewController.swift
+++ SectionRowsTutorial/Controllers/ThirdViewController.swift
@@ -18,6 +18,8 @@ class ThirdViewController: UIViewController {
     var weightTextField = UITextField()
     var weightLabel = UILabel()
     
+    var notesTextView = UITextView()
+    
     var repsTextField = UITextField()
     var repsLabel = UILabel()
     
@@ -39,7 +41,8 @@ class ThirdViewController: UIViewController {
         navConAcc()
         labelConfig()
         setTextFieldConstraints()
-//        setImageViewConstraints()
+        setImageViewConstraints()
+        setTextViewConstraints()
     }
     
     
@@ -68,40 +71,40 @@ class ThirdViewController: UIViewController {
         repsLabel.text = "  Repetitions: "
         repsLabel.textColor = .black
         
+        notesTextView.layer.borderWidth = 1
+        notesTextView.backgroundColor = .white
+        notesTextView.layer.cornerRadius = 25
+        notesTextView.layer.borderColor = UIColor.lightGray.cgColor
+        notesTextView.text = "Notes"
+        notesTextView.textAlignment = .center
+        
+        
         repsTextField.leftView = repsLabel
         repsTextField.leftViewMode = .always
         
-        view.addSubview(weightTextField)
-        view.addSubview(repsTextField)
+        [weightTextField, repsTextField, notesTextView].forEach{view.addSubview($0)}
+
     }
     
     //MARK: - TextField Constrainst
     func setTextFieldConstraints(){
-        weightTextField.translatesAutoresizingMaskIntoConstraints = false
-        weightTextField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -650).isActive = true
-        weightTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        weightTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        weightTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-        
-        repsTextField.translatesAutoresizingMaskIntoConstraints = false
-        repsTextField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -550).isActive = true
-        repsTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        repsTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        repsTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        weightTextField.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor,padding: .init(top: 20, left: 40, bottom: 0, right: -40), size: .init(width: 0, height: 50))
+        repsTextField.anchor(top: weightTextField.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 30, left: 40, bottom: 0, right: -40) ,size: .init(width: 0, height: 50))
     }
     
     //MARK: - ImageView Constraints
-//    func setImageViewConstraints(){
-//        timerImage.translatesAutoresizingMaskIntoConstraints = false
-//
-//        timerImage.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -450).isActive = true
-//        timerImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-//        timerImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-//        timerImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//    }
-    
-    
+    func setImageViewConstraints(){
+
+        timerImage.anchor(top: repsTextField.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 40, left: 0, bottom: 0, right: 0), size: .init(width: 100, height: 100))
+
+    }
+    //MARK: - TextView Constraints
+    func setTextViewConstraints(){
+
+        notesTextView.anchor(top: timerImage.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 40, left: 40, bottom: 0, right: -40), size: .init(width: 100, height: 110))
+        
+    }
     
     
     
@@ -115,7 +118,6 @@ class ThirdViewController: UIViewController {
     func timeClock(){
         let image1 = UIImage(named: "stopwatch")
         timerImage = UIImageView(image: image1)
-        timerImage.frame = CGRect(x: 160, y: 350, width: 100, height: 110)
         timerImage.contentMode = .scaleAspectFit
         self.view.addSubview(timerImage)
     }
@@ -136,9 +138,35 @@ class ThirdViewController: UIViewController {
         }
     }
     
-    
-    
-    
-    
-    
+}
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
 }
